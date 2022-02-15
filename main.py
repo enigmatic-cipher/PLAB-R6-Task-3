e81d75e79ddf9582ca8650571057fed4f179f500
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-02-15 10:59:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Given two different list as input. Print the sum of smallest and largest element from both the list.
+Input-> [1,5], [9,7]
+Output-> 10
+"""
+
+list_1 = [1,5]
+list_2 = [9,7]
+small_num = min(list_1)
+large_num = max(list_2)
+print(small_num + large_num)
